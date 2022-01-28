3c66e4ffb56437e040c526447492741da07667b7
Nikolay Gagara <ggn.f763@gmail.com>
2022-01-29 00:30:17 +0300

    search_test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const data = [
+  {name: 'Николай', id: 1},
+  {name: 'Василий', id: 2},
+  {name: 'Евгений', id: 3},
+]
+
 function App() {
+  
+  const [newData, setNewData] = useState(data)
+  const [value, setValue] = useState('')
+
+  const handelSearch = (e) => {
+    e.preventDefault()
+    const searchResult = data.filter(item => item.name.includes(value))
+    if (searchResult.length === 0) {
+      setNewData(data)
+    } else {
+      setNewData(searchResult)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+        <input type={"search"} onChange={(e)=>setValue(e.target.value)} value={value}/>
+        <button type={'submit'} onClick={(e)=>handelSearch(e)}>Искать</button>
+        </form>
+        <ul>
+          {newData.map(name => (
+            <li key={name.id}>{name.name}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
